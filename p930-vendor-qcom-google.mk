b7290cc470c6045cdae45692e0cf5aa102fd81c4
Californication <miroslav_mm@ukr.net>
2013-04-16 22:54:02 +0300

    first commit

---
+++ p930-vendor-qcom-google.mk
@@ -0,0 +1,16 @@
+## EGL
+PRODUCT_COPY_FILES += \
+    vendor/lge/p930/proprietary/qcom/google/system/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+    vendor/lge/p930/proprietary/qcom/google/system/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    vendor/lge/p930/proprietary/qcom/google/system/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/lge/p930/proprietary/qcom/google/system/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    vendor/lge/p930/proprietary/qcom/google/system/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+    vendor/lge/p930/proprietary/qcom/google/system/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    vendor/lge/p930/proprietary/qcom/google/system/lib/libOpenVG.so:system/lib/libOpenVG.so \
+    vendor/lge/p930/proprietary/qcom/google/system/lib/libgsl.so:system/lib/libgsl.so \
+    vendor/lge/p930/proprietary/qcom/google/system/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+    vendor/lge/p930/proprietary/qcom/google/system/lib/libC2D2.so:system/lib/libC2D2.so \
+    vendor/lge/p930/proprietary/qcom/google/system/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
+    vendor/lge/p930/proprietary/qcom/google/system/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+    vendor/lge/p930/proprietary/qcom/google/system/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+    vendor/lge/p930/proprietary/qcom/google/system/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
